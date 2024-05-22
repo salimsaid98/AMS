4930ff694c7971afb55f44a81369a1d2e86f6eee
salimsaid98 <saidhadimu@gmail.com>
2024-05-22 12:07:41 +0300

    production changes

--- ui/src/enviroment/enviroment.prod.ts
+++ ui/src/enviroment/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    url: 'http://172.21.0.1:8080/api/',
+    url: 'http://127.0.0.1:8080/api/',
   };
   
--- ui/src/enviroment/enviroment.ts
+++ ui/src/enviroment/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    url: 'http://localhost:8080/api/',
+    url: 'http://192.168.100.144:8080/api/',
   };
